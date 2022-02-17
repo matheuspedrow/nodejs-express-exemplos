434b8f06be2b762dac99fbcaae86f1688324e7f0
Matheus Pedro <matheuspedrow@gmail.com>
2022-02-17 16:22:41 -0300

    Tratando Erros

--- src/modules/user/userService.js
+++ src/modules/user/userService.js
@@ -4,10 +4,7 @@ const getUserByEmail = (searchEmail) =>
 users.find((obj) => obj.email === searchEmail)
 
 export const signup = (data) => {
-  if (getUserByEmail(data.email)) {
-    console.log('EXISTE O EMAIL')
-  } else {
+  if (getUserByEmail(data.email)) throw new Error ('email_existente')
     users.push(data)
+    return true
   }
-  return true
-}
